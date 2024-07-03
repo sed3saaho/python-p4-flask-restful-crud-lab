a5c67ad90502258a903c460afb209ee10fd37477
Sedly <sedly.keith@student.moringaschool.com>
2024-07-03 22:20:14 +0300

    python-p4-flask-restful-crud-lab

--- server/app.py
+++ server/app.py
@@ -47,6 +47,28 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        data = request.get_json()
+
+        plant = Plant.query.filter_by(id=id).first()
+
+        for attr in data:
+            if attr == 'is_in_stock':
+                plant.is_in_stock = data[attr]
+            else:
+                plant[attr] = data[attr]
+
+        db.session.add(plant)
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
